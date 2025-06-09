c33162499dc94ef1cb9389c158a25303c6369ffe
Anddy <anddysilva15@gmail.com>
2025-06-08 20:54:54 -0500

    Add: vehicle form and validations

--- src/main/java/com/registervehicle/vehicle/model/UserForm.java
+++ src/main/java/com/registervehicle/vehicle/model/UserForm.java
@@ -1,7 +1,5 @@
 package com.registervehicle.vehicle.model;
 
-import java.util.Date;
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
